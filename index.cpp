ae5b05ba6aac91f04bf67f6300142410856b6532
HarshitaKhare28 <kharehs@rknec.edu>
2023-11-10 20:34:04 +0530

    commit

--- index.cpp
+++ index.cpp
@@ -48,7 +48,7 @@ public:
 };
 class TicTacToe
 {
-private:
+protected:
     TwoDArray *board;
 
 public:
@@ -61,9 +61,9 @@ public:
         printf("\t\t\t Tic-Tac-Toe\n\n");
         printf("Choose a cell numbered from 1 to 9 as below and play\n\n");
         printf("\t\t\t 1 | 2 | 3 \n");
-        printf("\t\t\t--------------\n");
+        printf("\t\t\t-----------\n");
         printf("\t\t\t 4 | 5 | 6 \n");
-        printf("\t\t\t--------------\n");
+        printf("\t\t\t-----------\n");
         printf("\t\t\t 7 | 8 | 9 \n\n");
         printf("-\t-\t-\t-\t-\t-\t-\t-\t-\t-\n\n");
     }
@@ -336,105 +336,80 @@ public:
         }
     }
 };
-// some functions and variables for 2 player game
-//Array for the board
-char board[3][3] = {{'1','2','3'},{'4','5','6'},{'7','8','9'}};
-//Variable Declaration
-int choice;
-int row,column;
-char turn = 'X';
-bool draw = false;
-
-//Function to show the current status of the gaming board
-
-void display_board(){
-
-    //Rander Game Board LAYOUT
-
-    cout<<"PLAYER - 1 [X]\t\t\t PLAYER - 2 [O]\n";
-    cout<<"\n  "<<board[0][0]<<"  | "<<board[0][1]<<"  |  "<<board[0][2]<<" \n";
-    cout<<"----------------\n";
-    cout<<"  "<<board[1][0]<<"  | "<<board[1][1]<<"  |  "<<board[1][2]<<" \n";
-    cout<<"----------------\n";
-    cout<<"  "<<board[2][0]<<"  | "<<board[2][1]<<"  |  "<<board[2][2]<<" \n";
-   
-}
-
-//Function to get the player input and update the board
-
-void player_turn(){
-    if(turn == 'X'){
-        cout<<"Player - 1 [X] turn(Enter no. from 1 to 9) : ";
+//class for 2 player
+class TwoPlayer: public TicTacToe
+{
+    public: char player1 = HUMANMOVE;
+    public: char player2 = COMPUTERMOVE;
+    void play(){
+        int choice;
+        printf("\t\t\tThe Game Begins!");
+        showInstructions();
+        while(true){
+            mistake:
+            printf("\tYour Move\n");
+            printf("\tEnter Your Choice (Player 1): ");
+            scanf("%d", &choice);
+            if (board->getArrayElement(choice) != ' ')
+            {
+                printf("\nThis box has already been taken, please select a different box\n");
+                goto mistake;
+            }else{
+                board->updateArray(player1, choice);
+                printf("\nBoard after move\n");
+                showboard();
+                if (hasAnyoneWon() != 0)
+                    {
+                    switch (hasAnyoneWon())
+                        {
+                        case 1:
+                            printf("\t\t\tPlayer 2 Wins!\n");
+                            break;
+                        case -1:
+                            printf("\t\t\tPlayer 1 Wins!\n");
+                            break;
+                        }
+                    break;
+                    }
+                if(!isMovesLeft()){
+                    break;
+                }
+                mistake2:
+                printf("\tEnter Your choice (Player 2): ");
+                scanf("%d", &choice);
+                if (board->getArrayElement(choice) != ' ')
+                {
+                printf("\nThis box has already been taken, please select a different box\n");
+                goto mistake2;
+                }
+                board->updateArray(player2,choice);
+                printf("\nBoard after move\n");
+                showboard();
+                if (hasAnyoneWon() != 0)
+                    {
+                    switch (hasAnyoneWon())
+                        {
+                        case 1:
+                            printf("\t\t\tPlayer 2 Wins!\n");
+                            break;
+                        case -1:
+                            printf("\t\t\tPlayer 1 Wins!\n");
+                            break;
+                        }
+                    break;
+                    }
+                if(!isMovesLeft()){
+                    break;
+                }
+        } 
+        
+    } 
+        if (hasAnyoneWon() == 0)
+                {
+                    printf("\n\t\tIts a Tie!\n");
+                }
     }
-    else if(turn == 'O'){
-        cout<<"Player - 2 [O] turn(Enter no. from 1 to 9) : ";
-    }
-    //Taking input from user
-    //updating the board according to choice and reassigning the turn Start
-
-    cin>> choice;
-
-    //switch case to get which row and column will be update
-
-    switch(choice){
-        case 1: row=0; column=0; break;
-        case 2: row=0; column=1; break;
-        case 3: row=0; column=2; break;
-        case 4: row=1; column=0; break;
-        case 5: row=1; column=1; break;
-        case 6: row=1; column=2; break;
-        case 7: row=2; column=0; break;
-        case 8: row=2; column=1; break;
-        case 9: row=2; column=2; break;
-        default:
-            cout<<"Invalid Move\n";
-    }
-
-    if(turn == 'X' && board[row][column] != 'X' && board[row][column] != 'O'){
-        //updating the position for 'X' symbol if
-        //it is not already occupied
-        board[row][column] = 'X';
-        turn = 'O';
-    }else if(turn == 'O' && board[row][column] != 'X' && board[row][column] != 'O'){
-        //updating the position for 'O' symbol if
-        //it is not already occupied
-        board[row][column] = 'O';
-        turn = 'X';
-    }else {
-        //if input position already filled
-        cout<<"Box already filled!n Please choose another!!\n";
-        player_turn();
-    }
-    /* Ends */
-    display_board();
-}
-
-//Function to get the game status e.g. GAME WON, GAME DRAW GAME IN CONTINUE MODE
-
-bool gameover(){
-    //checking the win for Simple Rows and Simple Column
-    for(int i=0; i<3; i++)
-    if(board[i][0] == board[i][1] && board[i][0] == board[i][2] || board[0][i] == board[1][i] && board[0][i] == board[2][i])
-    return false;
-
-    //checking the win for both diagonal
-
-    if(board[0][0] == board[1][1] && board[0][0] == board[2][2] || board[0][2] == board[1][1] && board[0][2] == board[2][0])
-    return false;
-
-    //Checking the game is in continue mode or not
-    for(int i=0; i<3; i++)
-    for(int j=0; j<3; j++)
-    if(board[i][j] != 'X' && board[i][j] != 'O')
-    return true;
-
-    //Checking the if game already draw
-    draw = true;
-    return false;
-}
-
-
-// Driver function
+};
 int main()
 {
    TicTacToe t1;
@@ -456,21 +431,9 @@ int main()
         }
     t1.play(difficulty);
     }else{
-         cout<<"\t\t\tT I C K -- T A C -- T O E -- G A M E\t\t\t\n"; 
-        cout<<"\t\t\t\tFOR 2 PLAYERS\n";
-        while(gameover()){
-            display_board();
-            player_turn();
-            gameover();
-        }
-        if(turn == 'X' && draw == false){
-        cout<<"Congratulations!Player with '0' has won the game\n";
-        }
-        else if(turn == 'O' && draw == false){
-        cout<<"Congratulations!Player with 'X' has won the game\n";
-        }
-        else
-        cout<<"\nGAME DRAW!!!\n";
+        TwoPlayer t;
+        printf("\t\t\t\t\t\tFOR 2 PLAYERS\n");
+        t.play(); 
     }
     printf("\n\n\t\tThanks for playing!\n");
 }
